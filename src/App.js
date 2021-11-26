d632f2c790e50e086a5352e2b68169927ebb3493
Artem <nevadaltd88@gmail.com>
2021-11-27 00:47:08 +0200

    Creating forms
    + prevent re-rendering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import {useState, useRef} from 'react'
+import FormInput from './components/input.jsx'
 
 function App() {
+  // const [username, setUsername] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const data = new FormData(e.target)
+    console.log(Object.fromEntries(data.entries()))
+  }
+
   return (
-    <div className="App">
-      <h1>Siema!</h1>
+    <div className="app">
+      <form onSubmit={handleSubmit}>
+        <FormInput name='username' placeholder='Username' />
+        <FormInput name='email' placeholder='Email' />
+        <FormInput name='fullname' placeholder='Full Name' />
+        <FormInput name='sth' placeholder='Sth else' />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
---
+++ src/components/input.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './input.css'
+
+const FormInput = (props) => {
+	return (
+		<div className='formInput'>
+			{/*<label htmlFor="">Username</label>*/}
+			<input name={props.name}
+					 placeholder={props.placeholder}
+					 />
+		</div>
+	)
+}
+
+export default FormInput
