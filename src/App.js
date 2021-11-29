8d699de545649168ae797f1d39c60082e05f9f25
Artem <nevadaltd88@gmail.com>
2021-11-29 16:26:25 +0200

    finished
    + Input validation and Disp.Errors

--- src/App.js
+++ src/App.js
@@ -17,35 +17,46 @@ function App() {
       name: 'username',
       type: 'text',
       placeholder: 'Username',
-      label: 'Username'
+      errorMessage: "Username should be 3-16 characters and shouldn't include any special character! ",
+      label: 'Username',
+      pattern: "^[A-Za-z0-9]{3,16}$",
+      required: true
     },
     {
       id: 2,
       name: 'email',
-      type: 'text',
+      type: 'email',
       placeholder: 'Email',
-      label: 'Email'
+      errorMessage: 'It should be a valid e-mail addres!',
+      label: 'Email',
+      required: true
     },
     {
       id: 3,
       name: 'birthday',
-      type: 'text',
+      type: 'date',
       placeholder: 'Birthday',
-      label: 'Birthday'
+      label: 'Birthday',
     },
     {
       id: 4,
       name: 'password',
       type: 'password',
       placeholder: 'Password',
-      label: 'Password'
+      errorMessage: "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
+      label: 'Password',
+      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+      required: true
     },
     {
       id: 5,
       name: 'confirmPassword',
       type: 'password',
       placeholder: 'Confirm Password',
-      label: 'Confirm Password'
+      errorMessage: "Password don't match!",
+      label: 'Confirm Password',
+      pattern: values.password,
+      required: true
     },
   ]
 
@@ -64,6 +75,7 @@ console.log(values)
   return (
     <div className="app">
       <form onSubmit={handleSubmit}>
+      <h1>Register</h1>
         {inputs.map((input) => (
             <FormInput key={input.id}
                        {...input}
